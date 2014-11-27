ce90ad3b5ed7fcfaa8e3d80d8c4da329b690af59
Josh Chasky <jchasky@gmail.com>
2014-11-27 12:59:26 -0500

    shamu: disable secure discard
    Change-Id: I25168abc39d6c2fee51fa4f520a4382f38926e24

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,3 +167,6 @@ BOARD_USES_QC_TIME_SERVICES := true
 
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
+
+# Enable workaround for slow rom flash
+BOARD_SUPPRESS_SECURE_ERASE := true
